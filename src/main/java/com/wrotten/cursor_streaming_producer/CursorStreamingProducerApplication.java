61eaf66fa0e654c4a92ed879dbccf734ce6b30b8
Rtx 2060 <ronjayrudra98@gmail.com>
2024-06-26 00:29:43 +0530

    Initial Commit with empty project

---
+++ src/main/java/com/wrotten/cursor_streaming_producer/CursorStreamingProducerApplication.java
@@ -0,0 +1,13 @@
+package com.wrotten.cursor_streaming_producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursorStreamingProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursorStreamingProducerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wrotten/cursor_streaming_producer/CursorStreamingProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wrotten.cursor_streaming_producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursorStreamingProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
