7d2ea8ff180fbaa3e2990d16d0a7bc4b9089715f
lannisterblr18 <ronjayrudra98@gmail.com>
2024-07-08 04:03:10 +0530

    fixed frontend to match with the cursor streaming consumer service

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,18 +1,25 @@
-console.log("Hello world");
-const stompClient = new StompJs.Client({
-    brokerURL: 'ws://localhost:8080/ws'
-});
-stompClient.activate();
-
-
-const myFunction = (event) => {
-    console.log("Came here");
-    console.log(event)
-    stompClient.publish({
-        destination: "/app/stream-test",
-        body: JSON.stringify({
-            xPosition: event.offsetX,
-            yPosition: event.offsetY
-        })
+$(document).ready(function() {
+    console.log("Hello world");
+    const stompClient = new StompJs.Client({
+        brokerURL: 'ws://localhost:8080/ws'
     });
-}
+    stompClient.activate();
+    let count = 0;
+    const limit = 10;
+    $("#rectangle").mousemove(function (event) {
+        console.log("Came here");
+        console.log(event)
+        if (count >= limit) {
+            stompClient.publish({
+                destination: "/app/stream-test",
+                body: JSON.stringify({
+                    xPosition: event.clientX,
+                    yPosition: event.clientY
+                })
+            });
+            count = 0;
+        } else {
+            count += 1;
+        }
+    })
+});
